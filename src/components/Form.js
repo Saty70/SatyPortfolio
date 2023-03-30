651c9221204cc9f779bfc2133f3ea38111e74359
Satyam <skpfromgkp@gmail.com>
2023-03-30 18:00:50 +0530

    connected contact page to google sheets using nocodeapi

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,68 @@
 import "./formStyles.css";
 
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = () => {
-  return (
+  const[data, setData] = useState({
+    name:"",
+    email:"",
+    subject:"",
+    message:"",
+  });
+
+  const {name,email,subject,message}=data;
+
+  const handleChange = (e) =>{
+    setData({...data,[e.target.name]:e.target.value});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("https://v1.nocodeapi.com/saty10/google_sheets/ooqhbBxDMNrspBnf?tabId=Sheet1",{
+        method: 'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify([[name,email,subject,message, new Date().toLocaleString()]])
+      }
+    );
+      await response.json();
+      setData({...data,name:"",email:"",subject:"",message:""}); 
+    } catch (err) {
+      console.log(err)
+    }
+  };
+    return (
     <div className = "form">
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Your name</label>
-            <input type="text"></input>
+            <input type="text"
+            name="name"
+            autoComplete="off"
+            value={name}
+            onChange={handleChange}/>
+
             <label>E-mail</label>
-            <input type="email"></input>
+            <input type="email"
+            name="email"
+            autoComplete="off"
+            value={email}
+            onChange={handleChange}/>
+
             <label>Subject</label>
-            <input type="text"></input>
+            <input type="text"
+            name="subject"
+            autoComplete="off"
+            value={subject}
+            onChange={handleChange}/>
+
             <label>Message</label>
-            <textarea rows = "6" placeholder = "Type your message here"/>
+            <textarea rows = "6" placeholder = "Type your message here"
+            name="message"
+            autoComplete="off"
+            value={message}
+            onChange={handleChange}/>
             <button className="btn">Submit</button>
         </form>
     </div>
