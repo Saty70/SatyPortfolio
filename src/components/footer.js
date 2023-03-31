430e5d8c4559eaf2537cc6a859b6a380afc7b76c
Satyam <skpfromgkp@gmail.com>
2023-03-31 19:10:06 +0530

    added contact medias in about

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import "./footerStyles.css"
 import React from 'react'
 import { NavLink } from "react-router-dom";
 
-import {FaHome, /*FaPhone,*/ FaMailBulk, FaLinkedin, FaGithub, FaInstagram, FaStackOverflow} from "react-icons/fa";
+import {FaHome, /*FaPhoneAlt,*/ FaMailBulk, FaGithub, FaInstagram, FaStackOverflow, FaTwitter, FaTwitch, FaLinkedinIn} from "react-icons/fa";
 
 const footer = () => {
   return (
@@ -18,10 +18,10 @@ const footer = () => {
                     </div>
                 </div>
                 {/*<div className = "phone">
-                    <h4><FaPhone size={20} style={{color:"#fff", marginRight: "2rem"}}/>000-9999-999</h4>
+                    <h4><FaPhoneAlt size={20} style={{color:"#fff", marginRight: "2rem"}}/>000-9999-999</h4>
                 </div>*/}
                 <div className = "email">
-                    <h4><FaMailBulk size={20} style={{color:"#fff", marginRight: "2rem"}}/>hi@mailme.com</h4>
+                    <h4><FaMailBulk size={25} style={{color:"#fff", marginRight: "2rem"}}/>hi@mailme.com</h4>
                 </div>
             </div>
             <div className="right">
@@ -31,7 +31,7 @@ const footer = () => {
                 <p>Currently, I am a student at the National Forensics Sciences University, pursuing my Bachelor of Technology degree in Computer Science.</p>
                 <div className="social">
                 <NavLink to = "https://www.linkedin.com/in/satyam-kumar-prasad-b53ba9242/">
-                    <FaLinkedin size={30} style={{color:"#fff", marginRight: "1rem"}}/>
+                    <FaLinkedinIn size={30} style={{color:"#fff", marginRight: "1rem"}}/>
                 </NavLink>
 
 
@@ -46,6 +46,14 @@ const footer = () => {
 
                 <NavLink to = "https://stackoverflow.com/users/17837709/satyam-kumar-prasad">
                     <FaStackOverflow size={30} style={{color:"#fff", marginRight: "1rem"}}/>
+                </NavLink>
+
+                <NavLink to = "https://twitter.com/Saty_ikp">
+                    <FaTwitter size={30} style={{color:"#fff", marginRight: "1rem"}}/>
+                </NavLink>
+                
+                <NavLink to = "https://www.twitch.tv/saty_10">
+                    <FaTwitch size={30} style={{color:"#fff", marginRight: "1rem"}}/>
                 </NavLink>
                 
                 </div>
