2b13315464dade076750604afe00dd9057a3114f
Satyam <skpfromgkp@gmail.com>
2023-04-04 00:00:39 +0530

    hero edit

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -2,14 +2,14 @@ import "./HeroImgStyles.css";
 
 import React from 'react'
 
-//import IntroImg from "../assets/intro-bg.jpg"
+import IntroImg from "../assets/intro-bg.jpg"
 
 import {Link} from "react-router-dom";
  
 const HeroImg = () => {
   return <div className = "hero">
     <div className = "mask">
-        {/*<img className = "into-img" src= {IntroImg} alt="IntroImg"/>*/}
+        <img className = "into-img" src= {IntroImg} alt="IntroImg"/>
     </div>
     <div className = "content">
         <p>hi I'm  a Student</p>
