ef690795b43f535ebd1156f8252fcf1eefdf9e96
Satyam <skpfromgkp@gmail.com>
2023-04-03 20:06:18 +0530

    edited projects

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,29 +1,30 @@
-import cat from "../assets/cat.jpg"
+//import cat from "../assets/cat.jpg"
+import noteliness from "../assets/noteliness.jpg";
 //import catty from "../assets/crush.jpg"
 //import cat from "../assets/cat.jpg"
 
 const ProjectCardData = [
-    {
-        imgsrc: cat,
-        title : "title 1",
-        text : "about the project1",
-        view : "https://www.instagram.com/saty.ikp/",
-        src : "https://github.com/Saty70"
-    },
-    {
+    /*{
         imgsrc: cat,
         title : "title 2",
         text : "about the project2",
         view : "https://www.instagram.com/saty.ikp/",
         src : "https://github.com/Saty70"
-    },
+    },*/
     {
+        imgsrc: noteliness,
+        title : "noteliness",
+        text : "Instant class notes sharing app",
+        view : "https://github.com/Saty-riasis/Noteliness",
+        src : "https://github.com/Saty-riasis/Noteliness"
+    },
+    /*{
         imgsrc: cat,
         title : "title 3",
         text : "about the project3",
         view : "https://www.instagram.com/saty.ikp/",
         src : "https://github.com/Saty70"
-    },
+    },*/
     
 ]
 
